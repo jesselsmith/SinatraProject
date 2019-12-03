8d4086d3ed90df576ef2094e978f73a09bd7332a
Jesse <jesse.lee.smith@gmail.com>
2019-12-03 15:47:30 -0800

    setting up routes for CharacterController

---
+++ app/controllers/adventure_log_controller.rb
@@ -0,0 +1,3 @@
+class AdventureLogController < ApplicationController
+
+end
---
+++ app/controllers/character_controller.rb
@@ -0,0 +1,26 @@
+class CharacterController < ApplicationController
+  get '/characters' do
+
+  end
+
+  get '/characters/new' do
+
+  end
+
+  get '/characters/:slug' do
+
+  end
+
+  get '/characters/:slug/edit' do
+
+  end
+
+  get '/characters/:slug/delete' do
+
+  end
+
+  get '/characters/:slug/adventure-logs' do
+
+  end
+
+end
