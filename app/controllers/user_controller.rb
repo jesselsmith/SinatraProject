3e272be109c34bfc1f822b50fb57fe8889a8662f
Jesse <jesse.lee.smith@gmail.com>
2019-12-03 14:54:01 -0800

    Adding in logic for User routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,28 @@
 class UserController < ApplicationController
   get '/users/:slug' do
-
+    if logged_in?
+      @user =  User.find_by_slug(params[:slug])
+      if @user&.id == session[:user_id]
+        erb :'users/show'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/users/:slug/edit' do
-    
+    if logged_in?
+      @user =  User.find_by_slug(params[:slug])
+      if @user&.id == session[:user_id]
+        erb :'users/show'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/users/:slug/delete' do
