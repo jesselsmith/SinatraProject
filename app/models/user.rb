d87142430cd49338cc8ead9043d7843224c07d9f
Jesse <jesse.lee.smith@gmail.com>
2019-12-03 15:39:24 -0800

    Fixed error in associations, added destroy associations, and fixed up deletion logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :characters, dependent: :destroy
-  has_many :logs, through: :characters, dependent: :destroy
+  has_many :adventure_logs, through: :characters, dependent: :destroy
 
   extend Concerns::Slugifiable::ClassMethods
   include Concerns::Slugifiable::InstanceMethods
