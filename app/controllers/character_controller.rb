f94b6c611ba9f3c63364507dd4f5176bea2346a8
Jesse <jesse.lee.smith@gmail.com>
2019-12-03 21:02:31 -0800

    Adding logic to route for character creation and editing

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -21,8 +21,7 @@ class CharacterController < ApplicationController
   get '/characters/:slug' do
     if logged_in?
       @character = Character.find_by_slug(params[:slug])
-      @user = current_user
-      if @character&.user == @user
+      if @character&.user == current_user
         erb :'characters/show'
       else
         redirect '/dashboard'
@@ -33,18 +32,56 @@ class CharacterController < ApplicationController
   end
 
   get '/characters/:slug/edit' do
-
+    if logged_in?
+      @character = Character.find_by_slug(params[:slug])
+      if @character&.user == current_user
+        erb :'characters/edit'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/characters/:slug/delete' do
-
+    if logged_in?
+      @character = Character.find_by_slug(params[:slug])
+      if @character&.user == current_user
+        erb :'characters/delete'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/characters/:slug/adventure-logs' do
-
+    if logged_in?
+      @character = Character.find_by_slug(params[:slug])
+      if @character&.user == current_user
+        erb :'characters/adventure-logs'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   post '/characters' do
+    if logged_in?
+      user = current_user
+      if valid_character_hash(params)
+        character = user.characters.build(params)
+        redirect "/characters/#{character.slug}"
+      else
+        redirect '/characters/new'
+      end
+    else
+      redirect '/login'
+    end
 
   end
 
@@ -54,6 +91,11 @@ class CharacterController < ApplicationController
 
   delete '/characters/:slug' do
 
+  end
+
+  def valid_character_hash(hash)
+    !hash[:name].empty? && (hash[:level].is_a? Integer) &&
+    hash[:level].positive? && hash[:level] <= 20
   end
 
 end
