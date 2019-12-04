9995d961f7c805edcb10ef3e1a09e820c67b35be
Jesse <jesse.lee.smith@gmail.com>
2019-12-03 20:21:05 -0800

    Fixed character index route, added logic for indiviudal character route

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,14 +1,35 @@
 class CharacterController < ApplicationController
   get '/characters' do
-
+    if logged_in?
+      @user = current_user
+      @characters = @user.characters
+      erb :'characters/index'
+    else
+      redirect '/login'
+    end
   end
 
   get '/characters/new' do
-
+    if logged_in?
+      @user = current_user
+      erb :'characters/new'
+    else
+      redirect '/login'
+    end
   end
 
   get '/characters/:slug' do
-
+    if logged_in?
+      @character = Character.find_by_slug(params[:slug])
+      @user = current_user
+      if @character&.user == @user
+        erb :'characters/show'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/characters/:slug/edit' do
