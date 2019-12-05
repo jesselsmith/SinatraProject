0b6bf704debbb6557b9e3108d0c379a2ab6ceaea
Jesse <jesse.lee.smith@gmail.com>
2019-12-04 16:35:27 -0800

    Added back files accidentally left out

---
+++ app/controllers/adventure_logs_controller.rb
@@ -0,0 +1,33 @@
+class AdventureLogsController < ApplicationController
+  get '/adventure-logs/new' do
+  
+  end
+
+  get '/adventure-logs' do
+  
+  end
+
+  get '/adventure-logs/:id' do
+  
+  end
+
+  get '/adventure-logs/:id/edit' do
+  
+  end
+
+  get '/adventure-logs/:id/delete' do
+  
+  end
+
+  post '/adventure-logs' do
+
+  end
+
+  patch '/adventure-logs/:id' do
+  
+  end
+
+  delete '/adventure-logs/:id' do
+  
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,72 @@
+class UsersController < ApplicationController
+  get '/users/:slug' do
+    if logged_in?
+      @user = User.find_by_slug(params[:slug])
+      if @user&.id == session[:user_id]
+        erb :'users/show'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug/edit' do
+    if logged_in?
+      @user = User.find_by_slug(params[:slug])
+      if @user&.id == session[:user_id]
+        erb :'users/edit'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug/delete' do
+    if logged_in?
+      @user = User.find_by_slug(params[:slug])
+      if @user&.id == session[:user_id]
+        erb :'users/delete'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/users/:slug' do
+    if logged_in?
+      user = User.find_by_slug(params[:slug])
+      if user&.id == session[:user_id]
+        if !params[:user][:name].empty? && !params[:user][:email].empty? &&
+           !params[:user][:password].empty?
+          user.update(params[:user])
+        else
+          redirect "/users/#{params[:slug]}/edit"
+        end
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/users/:slug' do
+    if logged_in?
+      user = User.find_by_slug(params[:slug])
+      if user&.id == session[:user_id]
+        user.destroy
+        redirect '/logout'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+end
