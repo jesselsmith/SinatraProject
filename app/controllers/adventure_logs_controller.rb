101ff8bedfe2d7e221bc4e69c6b8c0000739cbcf
Jesse <jesse.lee.smith@gmail.com>
2019-12-05 16:11:00 -0800

    making adventure log creation work in routes

--- app/controllers/adventure_logs_controller.rb
+++ app/controllers/adventure_logs_controller.rb
@@ -57,9 +57,25 @@ class AdventureLogsController < ApplicationController
     end
   end
 
-
   post '/adventure-logs' do
+    if logged_in?
+      character = Character.find(params[:character])
+      if character&.user == current_user
+        log_hash = log_hasher(params)
+        if valid_log_hash(log_hash)
 
+          adventure_log = user.adventure_logs.build(log_hash)
+          adventure_log.save
+          redirect "/adventure_logs/#{adventure_log.slug}"
+        else
+          redirect '/adventure_logs/new'
+        end
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   patch '/adventure-logs/:id' do
@@ -68,5 +84,18 @@ class AdventureLogsController < ApplicationController
 
   delete '/adventure-logs/:id' do
   
+  end
+
+  # takes a hash from a form and returns a hash useful for creating a character
+  def log_hasher(hash)
+    log_hash = hash.except(:character)
+    log_hash[:gold_change] = hash[:gold_change].to_i
+    log_hash[:downtime_change] = hash[:downtime_change].to_i
+    log_hash[:level_up] = (hash[:level_up] == 'true')
+    log_hash
+  end
+
+  def valid_log_hash(hash)
+    !hash[:adventure_name].empty? && !hash[:dm_name].empty?
   end
 end
