9100630e353b5f026d50c443fd6cec94fd58103c
Jesse <jesse.lee.smith@gmail.com>
2019-12-06 12:58:47 -0800

    Added some of the functionality to the log model, then updated views with gold level downtime before and after!

--- app/models/adventure_log.rb
+++ app/models/adventure_log.rb
@@ -9,4 +9,28 @@ class AdventureLog < ActiveRecord::Base
   def gold_change
     self.gold_gained - self.gold_lost
   end
+
+  def level_before
+    self.character.level_before(self)
+  end
+
+  def level_after
+    level_before + (level_up? ? 1 : 0)
+  end
+
+  def gold_before
+    self.character.gold_before(self)
+  end
+
+  def gold_after
+    gold_before + gold_change
+  end
+
+  def downtime_before
+    self.character.downtime_before(self)
+  end
+
+  def downtime_after
+    downtime_before + downtime_change
+  end
 end
